96eb7f7b01ef05b14af92a0debabe0e082a7f822
Gigson Aquino <gigsonaquino4@gmail.com>
2025-12-09 11:51:51 +0800

    Walana dedo na

--- EmployeeRecordSystem/Form1.Designer.vb
+++ EmployeeRecordSystem/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,220 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnSeach = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblPosition = New System.Windows.Forms.Label()
+        Me.lblSalary = New System.Windows.Forms.Label()
+        Me.lblDepartment = New System.Windows.Forms.Label()
+        Me.lblSearch = New System.Windows.Forms.Label()
+        Me.dgvEmployees = New System.Windows.Forms.DataGridView()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtPosition = New System.Windows.Forms.TextBox()
+        Me.txtSalary = New System.Windows.Forms.TextBox()
+        Me.txtDepartment = New System.Windows.Forms.TextBox()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        CType(Me.dgvEmployees, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdate.Location = New System.Drawing.Point(689, 281)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(175, 49)
+        Me.btnUpdate.TabIndex = 1
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(689, 335)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(175, 49)
+        Me.btnDelete.TabIndex = 2
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLoad.Location = New System.Drawing.Point(689, 390)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(175, 49)
+        Me.btnLoad.TabIndex = 3
+        Me.btnLoad.Text = "Load"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnSeach
+        '
+        Me.btnSeach.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSeach.Location = New System.Drawing.Point(689, 445)
+        Me.btnSeach.Name = "btnSeach"
+        Me.btnSeach.Size = New System.Drawing.Size(175, 49)
+        Me.btnSeach.TabIndex = 4
+        Me.btnSeach.Text = "Search"
+        Me.btnSeach.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(689, 226)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(175, 49)
+        Me.btnAdd.TabIndex = 5
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(70, 45)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(70, 24)
+        Me.lblName.TabIndex = 6
+        Me.lblName.Text = "Name:"
+        '
+        'lblPosition
+        '
+        Me.lblPosition.AutoSize = True
+        Me.lblPosition.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPosition.Location = New System.Drawing.Point(61, 78)
+        Me.lblPosition.Name = "lblPosition"
+        Me.lblPosition.Size = New System.Drawing.Size(89, 24)
+        Me.lblPosition.TabIndex = 7
+        Me.lblPosition.Text = "Position:"
+        '
+        'lblSalary
+        '
+        Me.lblSalary.AutoSize = True
+        Me.lblSalary.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSalary.Location = New System.Drawing.Point(69, 112)
+        Me.lblSalary.Name = "lblSalary"
+        Me.lblSalary.Size = New System.Drawing.Size(74, 24)
+        Me.lblSalary.TabIndex = 8
+        Me.lblSalary.Text = "Salary:"
+        '
+        'lblDepartment
+        '
+        Me.lblDepartment.AutoSize = True
+        Me.lblDepartment.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDepartment.Location = New System.Drawing.Point(19, 146)
+        Me.lblDepartment.Name = "lblDepartment"
+        Me.lblDepartment.Size = New System.Drawing.Size(124, 24)
+        Me.lblDepartment.TabIndex = 9
+        Me.lblDepartment.Text = "Department:"
+        '
+        'lblSearch
+        '
+        Me.lblSearch.AutoSize = True
+        Me.lblSearch.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSearch.Location = New System.Drawing.Point(61, 170)
+        Me.lblSearch.Name = "lblSearch"
+        Me.lblSearch.Size = New System.Drawing.Size(79, 24)
+        Me.lblSearch.TabIndex = 10
+        Me.lblSearch.Text = "Search:"
+        '
+        'dgvEmployees
+        '
+        Me.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvEmployees.Location = New System.Drawing.Point(88, 226)
+        Me.dgvEmployees.Name = "dgvEmployees"
+        Me.dgvEmployees.Size = New System.Drawing.Size(542, 276)
+        Me.dgvEmployees.TabIndex = 11
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(146, 37)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(773, 32)
+        Me.txtName.TabIndex = 12
+        '
+        'txtPosition
+        '
+        Me.txtPosition.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPosition.Location = New System.Drawing.Point(146, 70)
+        Me.txtPosition.Name = "txtPosition"
+        Me.txtPosition.Size = New System.Drawing.Size(773, 32)
+        Me.txtPosition.TabIndex = 13
+        '
+        'txtSalary
+        '
+        Me.txtSalary.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSalary.Location = New System.Drawing.Point(146, 103)
+        Me.txtSalary.Name = "txtSalary"
+        Me.txtSalary.Size = New System.Drawing.Size(773, 32)
+        Me.txtSalary.TabIndex = 14
+        '
+        'txtDepartment
+        '
+        Me.txtDepartment.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDepartment.Location = New System.Drawing.Point(146, 138)
+        Me.txtDepartment.Name = "txtDepartment"
+        Me.txtDepartment.Size = New System.Drawing.Size(773, 32)
+        Me.txtDepartment.TabIndex = 15
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(146, 173)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(773, 32)
+        Me.txtSearch.TabIndex = 16
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(960, 552)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.txtDepartment)
+        Me.Controls.Add(Me.txtSalary)
+        Me.Controls.Add(Me.txtPosition)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.dgvEmployees)
+        Me.Controls.Add(Me.lblSearch)
+        Me.Controls.Add(Me.lblDepartment)
+        Me.Controls.Add(Me.lblSalary)
+        Me.Controls.Add(Me.lblPosition)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnSeach)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Employee Record"
+        CType(Me.dgvEmployees, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnSeach As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblPosition As Label
+    Friend WithEvents lblSalary As Label
+    Friend WithEvents lblDepartment As Label
+    Friend WithEvents lblSearch As Label
+    Friend WithEvents dgvEmployees As DataGridView
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtPosition As TextBox
+    Friend WithEvents txtSalary As TextBox
+    Friend WithEvents txtDepartment As TextBox
+    Friend WithEvents txtSearch As TextBox
 End Class
--- EmployeeRecordSystem/Form1.vb
+++ EmployeeRecordSystem/Form1.vb
@@ -1,3 +1,119 @@
-﻿Public Class Form1
+﻿
+Public Class Form1
 
+    Private employeesTable As New DataTable()
+    Private nextId As Integer = 1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        employeesTable.Columns.Add("ID", GetType(Integer))
+        employeesTable.Columns.Add("Name", GetType(String))
+        employeesTable.Columns.Add("Position", GetType(String))
+        employeesTable.Columns.Add("Salary", GetType(Decimal))
+        employeesTable.Columns.Add("Department", GetType(String))
+
+
+        dgvEmployees.DataSource = employeesTable
+
+
+        AddSampleData()
+    End Sub
+
+
+    Private Sub AddSampleData()
+        employeesTable.Rows.Add(nextId, "John Doe", "Manager", 50000.0, "HR")
+        nextId += 1
+        employeesTable.Rows.Add(nextId, "Jane Smith", "Developer", 45000.0, "IT")
+        nextId += 1
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If Not ValidateInputs() Then Return
+
+
+        employeesTable.Rows.Add(nextId, txtName.Text, txtPosition.Text, Convert.ToDecimal(txtSalary.Text), txtDepartment.Text)
+        nextId += 1
+
+        MessageBox.Show("Employee added successfully!")
+        ClearInputs()
+
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If dgvEmployees.SelectedRows.Count = 0 Then
+            MessageBox.Show("Please select an employee to update.")
+            Return
+        End If
+        If Not ValidateInputs() Then Return
+
+
+        Dim selectedRow As DataRow = employeesTable.Rows(dgvEmployees.SelectedRows(0).Index)
+
+
+        selectedRow("name") = txtName.Text
+        selectedRow("position") = txtPosition.Text
+        selectedRow("salary") = Convert.ToDecimal(txtSalary.Text)
+        selectedRow("department") = txtDepartment.Text
+
+        MessageBox.Show("Employee updated successfully!")
+        ClearInputs()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If dgvEmployees.SelectedRows.Count = 0 Then
+            MessageBox.Show("Please select an employee to delete.")
+            Return
+        End If
+
+
+        employeesTable.Rows.RemoveAt(dgvEmployees.SelectedRows(0).Index)
+
+        MessageBox.Show("Employee deleted successfully!")
+        ClearInputs()
+
+        dgvEmployees.Refresh()
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+
+        employeesTable.DefaultView.RowFilter = ""
+    End Sub
+
+    Private Sub btnSeach_Click(sender As Object, e As EventArgs) Handles btnSeach.Click
+
+        employeesTable.DefaultView.RowFilter = $"name LIKE '%{txtSearch.Text}%' OR Convert(id, 'System.String') LIKE '%{txtSearch.Text}%'"
+    End Sub
+
+
+
+    Private Sub dgvEmployees_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvEmployees.CellContentClick
+
+        If e.RowIndex >= 0 Then
+            Dim row As DataGridViewRow = dgvEmployees.Rows(e.RowIndex)
+            txtName.Text = row.Cells("name").Value.ToString()
+            txtPosition.Text = row.Cells("position").Value.ToString()
+            txtSalary.Text = row.Cells("salary").Value.ToString()
+            txtDepartment.Text = row.Cells("department").Value.ToString()
+        End If
+    End Sub
+
+    Private Function ValidateInputs() As Boolean
+        If String.IsNullOrWhiteSpace(txtName.Text) Or String.IsNullOrWhiteSpace(txtPosition.Text) Or
+           String.IsNullOrWhiteSpace(txtSalary.Text) Or String.IsNullOrWhiteSpace(txtDepartment.Text) Then
+            MessageBox.Show("All fields are required.")
+            Return False
+        End If
+        If Not Decimal.TryParse(txtSalary.Text, Nothing) Then
+            MessageBox.Show("Salary must be a valid number.")
+            Return False
+        End If
+        Return True
+    End Function
+
+
+    Private Sub ClearInputs()
+        txtName.Clear()
+        txtPosition.Clear()
+        txtSalary.Clear()
+        txtDepartment.Clear()
+    End Sub
 End Class
